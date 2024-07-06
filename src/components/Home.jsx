535e32ef60751a172817e36d8cb4ada449f97ef3
Ananya Bhatnagar <bhatnagarananya64@gmail.com>
2024-07-06 10:27:47 +0530

    update resume

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
-import BackgroundWrapper from './Backgroundwrapper';
 import { Link } from 'react-scroll';
 
 const Home = () => {
